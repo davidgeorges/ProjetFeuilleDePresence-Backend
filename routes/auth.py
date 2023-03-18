f2ef589b09c3301378d6f43819035fd4d1c4e159
David GEORGES <georges.david.jk@gmail.com>
2023-03-18 01:28:30 +0100

    Review 1.1

--- routes/auth.py
+++ routes/auth.py
@@ -30,144 +30,98 @@ async def login(user_receive: UserIn = Body(...)):
 
     db = database.get_db()
 
-    #Init status code & content
-    status_code = 500
-    detail = "Internval error, please retry."
-    errorMessage = ""
+    try:
 
-    try :
-        #Convert pydantic model to a Dict
         user = jsonable_encoder(user_receive)
-        #Get the user from database
         user_from_db = await db["users"].find_one({"email": user["email"]})
-        #If user exist
-        if ( user_from_db ) is not None:
-            #Check password from user and database
-            if verify_password(user["password"],user_from_db["hashed_password"]) :
-                try :   
-                    #Get the user role as a string
-                    user_role = userRoleAsString(user_from_db["role_id"])
-                    #Create token and refresh token with user id and role
-                    access_token = "Bearer "+token.create_token(user_from_db["_id"],user_role,"access")
-                    refresh_token = token.create_token(user_from_db["_id"],user_role,"refresh")
-                    try :
-                        #Insert refresh_token in db
-                        await db["users"].update_one({"_id":user_from_db["_id"]},{"$set":{"refresh_token":refresh_token}})
-                        refresh_token = "Bearer "+refresh_token
-                        response = JSONResponse(content={"message" :"User connected with success.","id":user_from_db["_id"], "role" : user_role},status_code=status.HTTP_200_OK)
-                        response.set_cookie(key="access_token",value=access_token,httponly=True)
-                        response.set_cookie(key="refresh_token",value=refresh_token,httponly=True)
-                        return response
-                    except Exception as e :
-                        errorMessage = f"Error while inserting token in db for user with id : {user_from_db['_id']} --> {str(e)}"
-                except Exception as e :
-                    errorMessage = f"Error while creating token in db for user with id : {user_from_db['_id']} --> {str(e)}"
-            else:   
-                status_code = 401
-                detail = "Wrong credentials."
-                errorMessage = f"Error wrong credentials for user with id : {user_from_db['_id']}"
-        else :
-            status_code = 400
-            detail = "User doesn't exist, please contact an admin."
-            errorMessage = f"Error user trying to connect with non existing account, email used : {user_receive['email']}"
-    except Exception as e : 
-        errorMessage = f"Error while convert pydantic to dict --> {str(e)}"
 
-    try :
-        error.write_in_file(errorMessage)
-    except Exception as e : 
-        print("Error while trying to write in file --> "+str(e))
-    return JSONResponse(detail, status_code)
+        if ( user_from_db ) is None:
+            raise HTTPException(400, "User doesn't exist, please contact an admin.")
+
+        if not verify_password(user["password"],user_from_db["hashed_password"]) :
+            raise HTTPException(401, "Wrong credentials.")
+            
+        user_role = userRoleAsString(user_from_db["role_id"])
+        access_token = "Bearer "+token.create_token(user_from_db["_id"],user_role,"access")
+        refresh_token = token.create_token(user_from_db["_id"],user_role,"refresh")
+        await db["users"].update_one({"_id":user_from_db["_id"]},{"$set":{"refresh_token":refresh_token}})
+        refresh_token = "Bearer "+refresh_token
+        response = JSONResponse(content={"message" :"User connected with success.","id":user_from_db["_id"], "role" : user_role},status.HTTP_200_OK)
+        response.set_cookie(key="access_token",value=access_token,httponly=True)
+        response.set_cookie(key="refresh_token",value=refresh_token,httponly=True)
+        return response
+     
+    except Exception as error : 
+        error.write_in_file(str(error))
+        raise HTTPException(500)
+
 
 @auth.post("/logout")
 async def logout():
-    response = JSONResponse("User disconnected with success.",status.HTTP_200_OK)
-    response.delete_cookie("access_token")
-    response.delete_cookie("refresh_token")
-    return response
+    return JSONResponse("User disconnected with success.", status.HTTP_200_OK).delete_cookie("access_token").delete_cookie("refresh_token")
+
+    
 
 @auth.get("/authStatus")
 async def auth_status(request : Request):
 
     db = database.get_db()
 
-    #Init status code & content
-    status_code = 401
-    detail = ""
-    errorMessage = ""
-
     try : 
+
         #Get the refresh_token from cookies
         refresh_token = request.cookies["refresh_token"]
-        try :
-            #If we have an user with the refresh_token
-            user_from_db = await db["users"].find_one({"refresh_token":refresh_token.replace("Bearer ","")},{"email", "role_id"})
-            #Get the user role as a string
-            user_role = userRoleAsString(user_from_db["role_id"])
-            #Check if is not expired
-            if(token.check_if_is_expired(refresh_token, "refresh") == "OK"):
-                return JSONResponse(content={"message" :"User already connected.","id":user_from_db["_id"], "role" : user_role},status_code=status.HTTP_200_OK)
-            else:
-                detail = "Error refresh_token expired."
-                errorMessage = "Error an user tried to reconnect with an expired refresh_token."
-        except :
-            detail = "Error no user with this refresh_token in the database."
-            errorMessage = "Error an user tried to reconnect with an non existing refresh_token."
-    except :
-        detail =  "No access - Missing refresh_token."
-        errorMessage =  "Error an user tried to reconnect without  refresh_token."
 
+        #If we have an user with the refresh_token
+        user_from_db = await db["users"].find_one({"refresh_token":refresh_token.replace("Bearer ","")},{"email", "role_id"})
 
-    try :
-        error.write_in_file(errorMessage)
-    except Exception as e : 
-        print("Error while trying to write in file --> "+str(e))
-    return JSONResponse(detail, status_code)
+        #Get the user role as a string
+        user_role = userRoleAsString(user_from_db["role_id"])
+
+        #Check if is not expired
+        if(token.check_if_is_expired(refresh_token, "refresh") != "OK"):
+            raise HTTPException(401,"Error refresh_token expired.")
+
+        return JSONResponse(content={"message" :"User already connected.","id":user_from_db["_id"], "role" : user_role},status.HTTP_200_OK)
+
+    except Exception as error :
+        error.write_in_file(error)
+        raise HTTPException(500)
+
 
 @auth.get("/refreshToken")
 async def refresh_token(request : Request):
 
     db = database.get_db()
 
-    #Init status code & content
-    status_code = 401
-    detail = "Internval error, please retry."
-    errorMessage = ""
-    
     try : 
+
         #Get the refresh_token from cookies
         refresh_token = request.cookies["refresh_token"]
-        try : 
-            #Get user from the db
-            user_from_db = await db["users"].find_one({"refresh_token" : refresh_token.replace("Bearer ", "")},{"email", "role_id"})
-            #If user exist
-            if user_from_db is not None:
-                #Check if the refresh token is valid
-                if(token.check_if_is_expired(refresh_token, "refresh") == "OK"):
-                    #Get the user role as a string
-                    user_role = userRoleAsString(user_from_db["role_id"])
-                    #Create the access token
-                    access_token = "Bearer " +token.create_token(user_from_db["_id"], user_role, "access")
-                    response = JSONResponse(content={"message": "Refresh token success.", "id":user_from_db["_id"],"role": user_role},status_code=status.HTTP_200_OK)
-                    response.set_cookie(key="access_token",value=access_token,httponly=True)
-                    return response
-                else:
-                    #Invalid refresh token
-                    detail = "Error refresh_token expired."
-                    errorMessage = "Error an user tried to reconnect with an expired refresh_token."
-            else:
-                detail = "Error no user with this refresh_token in the database."
-                errorMessage = "Error an user tried to reconnect with an non existing refresh_token."
-        except Exception as e:
-            status_code = 500
-            errorMessage = f"Error while convert pydantic to dict --> {str(e)}"
-    except :
-        detail =  "No access - Missing refresh_token."
-        errorMessage =  "No access - Missing refresh_token."
 
-    try:
-        error.write_in_file(errorMessage)
-    except Exception as e : 
-        print("Error while trying to write in file --> "+str(e))
-    return JSONResponse(detail, status_code)
+        #Get user from the db
+        user_from_db = await db["users"].find_one({"refresh_token" : refresh_token.replace("Bearer ", "")},{"email", "role_id"})
+
+        #If user don't exist
+        if user_from_db is None:
+            raise HTTPException(400,"Error no user with this refresh_token in the database.")
+
+        #Check if the refresh token is valid
+        if(token.check_if_is_expired(refresh_token, "refresh") != "OK"):
+            raise HTTPException(401,"Error refresh_token expired.")
+                    
+        #Get the user role as a string
+        user_role = userRoleAsString(user_from_db["role_id"])
+
+        #Create the access token
+        access_token = "Bearer " +token.create_token(user_from_db["_id"], user_role, "access")
+        response = JSONResponse(content={"message": "Refresh token success.", "id":user_from_db["_id"],"role": user_role},status.HTTP_200_OK)
+        response.set_cookie(key="access_token",value=access_token,httponly=True)
+            
+        return response
+
+    except Exception as error :
+        error.write_in_file(error)
+        raise HTTPException(500)
+
 
--- routes/student.py
+++ routes/student.py
@@ -19,73 +19,30 @@ To add  :
     Vérifiez si ce n'est pas férié ou les vacances...
 """
 @student.get('/setMyStatus/{dailyToken}')
-async def set_student_status(dailyToken : str,request: Request):
-
+async def set_student_status(dailyToken: str, request: Request):
 
     db = database.get_db()
-    
-    #Init status code & content
-    status_code = 500
-    detail = "Internval error, please retry."
-    errorMessage = ""
-    
-    # Check if it's a weekday
-    if datetime.today().weekday() < 5:  
-        #Get the access_token from cookies
-        access_token = request.cookies["access_token"]
-        
-        #Get the user id
-        student_id = token.get_data("id",access_token,"access")
+    access_token = request.cookies.get("access_token")
 
-        #If we have any id from token
-        if student_id is not None : 
-            try : 
-                #Get the student from is id
-                student_from_db = await db["users"].find_one({"_id" : student_id},{"promo_id","status"})
-                todayDateAndMonth = datetime.today().strftime("%d-%m")
-                #Check if the user has not already justified his presence
-                if todayDateAndMonth not in student_from_db["status"].keys() : 
-                    try : 
-                        #Get the promo linked to the student
-                        promo_from_db = await db["promo"].find_one({"_id": student_from_db["promo_id"]},{"daily_token"})
-                        #If the promo exist
-                        if promo_from_db is not None : 
-                            #Check if the daily_token of the user entry is the same as the one in the database
-                            if promo_from_db["daily_token"] == dailyToken :
-                                try : 
-                                    #Concat dict
-                                    statusUpdated = dict({todayDateAndMonth:"present"},**student_from_db["status"])
-                                    try : 
-                                        #Update in database
-                                        await db["users"].update_one({"_id":student_id},{"$set":{"status":statusUpdated}})
-                                        return JSONResponse("Success",status.HTTP_201_CREATED)
-                                    except Exception as e : 
-                                        detail = "Error, please try later."
-                                        errorMessage = f"Error while updating status for user with id : {student_id} and promo id : {student_from_db['promo_id']}"
-                                except Exception as e :
-                                    errorMessage = f"Error while concatenating dict for user with id : {student_id} and promo id : {student_from_db['promo_id']} --> {str(e)}"
-                            else : 
-                                detail = "Unvalid token, please retry."
-                                return JSONResponse(detail,status.HTTP_202_ACCEPTED)
-                        else :
-                            detail = "You are linked to a non-existent promotion, please contact an admin."
-                            errorMessage = f"Error non-existent promo for user with id : {student_id} and promo id : {student_from_db['promo_id']}"
-                    except Exception as e :
-                        errorMessage = f"Error while getting promo with id : {student_from_db['promo_id']} for user with id {student_id}"
-                else :
-                    detail = "You have already declared your presence."
-                    return JSONResponse(detail,status.HTTP_202_ACCEPTED)
-            except Exception as e :
-                errorMessage = f"Error while getting student with id  :  {student_id}"
-        else :
-            errorMessage = f"Error while decoding no id in the token : {access_token}"  
-    else:  
-        detail = "You cannot declare your presence on weekends."
-        return JSONResponse(detail,status.HTTP_202_ACCEPTED)
+    student_id = token.get_data("id", access_token, "access")
+    if not student_id:
+    return JSONResponse("Invalid access token", status.HTTP_401_UNAUTHORIZED)
 
-    
-    try :
-        error.write_in_file(errorMessage)
-    except Exception as e : 
-        print("Error while trying to write in file --> "+str(e))
-    return JSONResponse(detail, status_code)
+    student = await db["users"].find_one({"_id": student_id}, {"promo_id", "status"})
+    if not student:
+    return JSONResponse("Student not found", status.HTTP_404_NOT_FOUND)
+
+    today = datetime.today().strftime("%d-%m")
+    if today in student["status"]:
+    return JSONResponse("You have already declared your presence.", status.HTTP_400_BAD_REQUEST)
+
+    promo = await db["promo"].find_one({"_id": student["promo_id"]}, {"daily_token"})
+    if not promo:
+    return JSONResponse("Promo not found", status.HTTP_404_NOT_FOUND)
+
+    if promo["daily_token"] != dailyToken:
+    return JSONResponse("Invalid daily token", status.HTTP_400_BAD_REQUEST)
+
+    status_updated = {today: "present", **student["status"]}
+    await db["users"].update_one({"_id": student_id}, {"$set": {"status": status_updated}})
+    return JSONResponse("Success", status.HTTP_201_CREATED)
